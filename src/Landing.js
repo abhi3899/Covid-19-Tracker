5fe80078878e16ab4a6fb352cac2af865d3e6a1f
abhi3899 <abhism3899@gmail.com>
2020-09-07 12:17:58 +0530

    developer added

---
+++ src/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Landing.css'
+
+const Landing = () => {
+    return (
+        <div>
+            <a href="./App.js">Goto Tracker</a>
+        </div>
+    )
+}
+
+export default Landing
